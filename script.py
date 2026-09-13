3903abe09e499e542c7b6a5b4b0e6ff4da2bec3d
cd724 <cd896563@gmail.com>
2026-09-13 14:10:03 +0200

    Update URL for TikTok likes site

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from playwright.async_api import async_playwright
 # =====================================================================
 # CONFIGURATION : REMETTEZ VOS DEUX LIGNES ICI
 # =====================================================================
-URL_DU_SITE = "https://zefame.com/en/free-tiktok-likes"
+URL_DU_SITE = "https://leofame.com/free-tiktok-likes"
 LIEN_A_COLLER = "https://vm.tiktok.com/ZN8jfjrSm/"
 # =====================================================================
 
